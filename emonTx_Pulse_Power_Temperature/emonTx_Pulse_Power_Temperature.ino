f76cd6d0b60601502d0fe3e40a58e5346fc9cbdd
Jerome <jerome@jolimont.fr>
2013-06-14 00:06:25 +0200

    Pulse_Power_Temperature: Power down + wake on interrupt

--- emonTx_Pulse_Power_Temperature/emonTx_Pulse_Power_Temperature.ino
+++ emonTx_Pulse_Power_Temperature/emonTx_Pulse_Power_Temperature.ino
@@ -1,37 +1,66 @@
+// **** INCLUDES *****
+#include "LowPower.h"
+
+// Use pin 3 as wake up pin
+const int wakeUpPin = 3;
+const int LEDpin = 9;
 boolean interrupt_received = false;
+
+void wakeUp()
+{
+    // Just a handler for the pin interrupt.
+    interrupt_received = true;
+}
 
 void setup()
 {
+  pinMode(LEDpin, OUTPUT);   
+  digitalWrite(LEDpin, HIGH);
 
-  // Setup pulse counting interrupt
-  pinMode(3, INPUT);       // set interrupt0 (pin3) as input
-  digitalWrite(3, HIGH);   // and enable it's internal pull-up resistor
-  attachInterrupt(1, countP, FALLING);  // device signals a low when active
-  delay(100);
+    // Configure wake up pin as input.
+    // This will consumes few uA of current.
+    pinMode(wakeUpPin, INPUT);   
+    digitalWrite(wakeUpPin, HIGH);   // and enable it's internal pull-up resistor
+
+    digitalWrite(LEDpin, HIGH); delay(500); digitalWrite(LEDpin, LOW); delay(500);     // flash LED
 
   // Setup serial link
   Serial.begin(57600);
-  Serial.println ("Start");   // signal initalization done
+  Serial.println ("Start");
+  delay(10); // without the delay, the message is lost when going to sleep
 }
 
-void loop()
+void loop() 
 {
-  static unsigned long count = 0;
-  
-  // Software debounce (10 ms)
-  if (interrupt_received == true){
-    delay(10);
-    if (digitalRead(3) == LOW){
-      // Debounced interrupt action
-      count++;
-      Serial.println (count);
+    static unsigned long count = 0;
+
+    // Allow wake up pin to trigger interrupt on falling edge
+    attachInterrupt(1, wakeUp, FALLING);
+    
+    // Enter power down state with ADC and BOD module disabled.
+    // Wake up when wake up pin is low.
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    
+    // Disable external pin interrupt on wake up pin.
+    detachInterrupt(0); 
+    
+    // Software debounce (10 ms)
+    if (interrupt_received == true){
+      delay(10);
+      if (digitalRead(3) == LOW){
+        // Debounced interrupt action
+        count++;
+        Serial.println (count);
+        delay(10);
+      }
+      interrupt_received = false;
+      // Blink once per count
+      for (int i=0;i<count;i++)
+      {
+        digitalWrite(LEDpin, HIGH); delay(100); digitalWrite(LEDpin, LOW); delay(100);     // flash LED
+      }
     }
-    interrupt_received = false;
-  }
-}
-
-void countP()
-{
-  interrupt_received = true;
+    // Blink longer.
+    digitalWrite(LEDpin, HIGH); delay(500); digitalWrite(LEDpin, LOW); delay(500);     // flash LED
 }
 
