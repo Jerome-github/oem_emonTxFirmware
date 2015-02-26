ae11d3ca197790282342af6b3f8c5b798445f8bf
Jerome Lafréchoux <jerome@antispam.fake>
2015-02-26 16:57:04 +0100

    emonTx Pulse: remove power computation

--- emonTxV2/emonTx_Pulse/emonTx_Pulse.ino
+++ emonTxV2/emonTx_Pulse/emonTx_Pulse.ino
@@ -12,8 +12,6 @@
 
   The code below detects the falling edge of each pulse and increments pulseCount.
   
-  It calculates the power by calculating the time elapsed between pulses.
-  
   Read more about pulse counting here:
   http://openenergymonitor.org/emon/buildingblocks/introduction-to-pulse-counting
  
@@ -59,7 +57,10 @@ const int networkGroup = 210;                                           // emonT
 #include <JeeLib.h>                                                     // make sure V12 (latest) is used if using RFM69CW
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
   
-typedef struct { int power, pulse;} PayloadTX;
+typedef struct { 
+  int pulse;
+  } PayloadTX;
+
 PayloadTX emontx;                                                       // neat way of packaging data for RF comms
 
 const int pulsePin = 3;                                                 // Sensor triggers pulses on IRQ 1 = pin 3
@@ -68,10 +69,7 @@ const int debouncing_time = 10;                                         // Set i
 
 // Pulse counting settings 
 unsigned long pulseCount = 0;                                           // Number of pulses, used to measure energy.
-unsigned long pulseTime,lastTime;                                       // Used to measure power.
 unsigned long newPulse, lastPulse;                                      // Used to debounce interrupt
-double power = 0;                                                       // power
-int ppwh = 1;                                                           // 1000 pulses/kwh = 1 pulse per wh - Number of pulses per wh - found or set on the meter.
 
 
 void setup() 
@@ -103,13 +101,10 @@ void setup()
 
 void loop() 
 { 
-  emontx.power = power;
   emontx.pulse = pulseCount;
   send_rf_data();  // *SEND RF DATA* - see emontx_lib
 
 #ifdef DEBUGGING
-  Serial.print(emontx.power);
-  Serial.print("W ");
   Serial.println(emontx.pulse);
   digitalWrite(LEDpin, HIGH); delay(2); digitalWrite(LEDpin, LOW);      // flash LED
 #endif
@@ -124,10 +119,7 @@ void onPulse()
   newPulse = micros();
   if(newPulse - lastPulse >= debouncing_time * 1000) {
     lastPulse = newPulse;                                                 
-    lastTime = pulseTime;                                                 // used to measure time between pulses.
-    pulseTime = newPulse;
     pulseCount++;                                                         // pulseCounter               
-    power = int((3600000000.0 / (pulseTime - lastTime))/ppwh);            // Calculate power
 #ifdef DEBUGGING
     digitalWrite(LEDpin, HIGH); delay(10);                               // flash LED 
     digitalWrite(LEDpin, LOW); delay(10);     
